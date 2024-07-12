2bf6f9f86d5bba9ebdd5249b51feca4f01d5e81b
Jeanvally G.Beato <jeanvallybeato@gmail.com>
2024-07-11 22:07:53 -0700

    Can toggle read status & remove book

--- library.js
+++ library.js
@@ -34,32 +34,63 @@ function clearForm() {
     read_status.checked = false;
 }
 
-//Show library
-function printBooks() {
-    booksContainer.textContent = '';
-    myLibrary.forEach(book => {
+//Render library to page
+function renderLibrary(){
+    const books = document.querySelectorAll('.book');
+    books.forEach(book => booksContainer.removeChild(book));
 
-        const div = document.createElement('div');
-        const title = document.createElement('h4');
-        const author = document.createElement('p');
-        const pages = document.createElement('p');
-        const read_status = document.createElement('p');
+    for(let i=0; i < myLibrary.length; i++){
+        createBooks(myLibrary[i]);
+    }
+}
 
-        title.innerText = `Title: ${book.title}`;
-        author.innerText = `Author: ${book.author}`;
-        pages.innerText = `Pages: ${book.pages}`;
-        read_status.innerText = `Read ?: ${book.read_status}`;
+//Create library
+function createBooks(book) {
 
-        //may need to add more classes
-        div.classList = 'card'
+    const book_div = document.createElement('div');
+    const title = document.createElement('h4');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const read_button = document.createElement('button');
+    const remove_button = document.createElement('button');
 
-        div.appendChild(title);
-        div.appendChild(author);
-        div.appendChild(pages);
-        div.appendChild(read_status);
+    title.innerText = `Title: ${book.title}`;
+    author.innerText = `Author: ${book.author}`;
+    pages.innerText = `Pages: ${book.pages}`;
+    
+    read_button.classList.add('read_button')
+    
+    remove_button.textContent = 'Remove';
+    remove_button.setAttribute('id', 'remove_button');
 
-        booksContainer.appendChild(div);
+    //may need to add more classes
+    book_div.classList = 'book'
+
+    book_div.appendChild(title);
+    book_div.appendChild(author);
+    book_div.appendChild(pages);
+    book_div.appendChild(read_button);
+    if(book.read_status===false){
+        read_button.textContent = 'Not Read';
+        read_button.style.backgroundColor = '#e68510';
+    }
+    else {
+        read_button.textContent = 'Read';
+        read_button.style.backgroundColor = '#48e064';
+    }
+    book_div.appendChild(remove_button);
+
+    remove_button.addEventListener('click', () => {
+        myLibrary.splice(myLibrary.indexOf(book),1);
+        renderLibrary();
     });
+
+    read_button.addEventListener('click', () =>{
+        book.read_status = !book.read_status;
+        renderLibrary();
+    });
+
+    booksContainer.appendChild(book_div);
 }
 
 //Click Events
@@ -77,22 +108,22 @@ modal.addEventListener('submit', (e)=> {
     const book = new Book(title.value, author.value, pages.value, read_status.checked);
     myLibrary.push(book);
     clearForm();
-    printBooks();
+    renderLibrary();
 });
 
 
 //NEED TO FIGURE OUT WHY THE SUBMIT BUTTON IS NOT ADDING BOOK TO ARRAY
 
-let myBook1 = new Book('hello', 'world', 15, true);
+let myBook1 = new Book('hello', 'world', 15, false);
 addBookToLibrary(myBook1);
 
-let myBook2 = new Book('good', 'afternoon', 15, true);
+let myBook2 = new Book('good', 'afternoon', 15, false);
 addBookToLibrary(myBook2);
 
 let myBook3 = new Book('good', 'evening', 15, true);
 addBookToLibrary(myBook3);
 
-printBooks();
+renderLibrary();
 
 
 /* FOR TESTING ONLY - DELETE BELOW */
